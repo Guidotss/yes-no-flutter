c90d0c218e0be815937dcef21a77c313aaf31fb3
Guidotss <guidoolguin@hotmail.com>
2023-07-09 13:03:35 -0300

    Initial commmit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yes No App',
+      theme: AppTheme().theme(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: FilledButton.tonal(
+            child: const Text("Click me!"),
+            onPressed: () {},
+          )
+        ),
+      ),
+    );
+  }
+}
