8ce7330355ae28e272aa132477108cd23e9d4246
Guidotss <guidoolguin@hotmail.com>
2023-07-10 17:14:34 -0300

    Terminando Yes No App

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 
 class HerMessageBubble extends StatelessWidget {
-  const HerMessageBubble({super.key});
+  
+  final Message message; 
+  const HerMessageBubble({
+    super.key,
+    required this.message
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,45 +20,48 @@ class HerMessageBubble extends StatelessWidget {
             color: colos.secondary,
             borderRadius: BorderRadius.circular(20)
           ),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               child: Text(
-                "Hola mundo",
-                style: TextStyle(color: Colors.white),
+                message.text,
+                style: const TextStyle(color: Colors.white),
               ),
             ),
-          ),
-          const SizedBox(height: 5),
-
-          _ImageBubble() 
-
-          //TODO: Add image
+        ),
+        const SizedBox(height: 5),
+        _ImageBubble(imageUrl: message.imageUrl!,)
       ],
     ); 
   }
 }
 class _ImageBubble extends StatelessWidget {
   
+  final String imageUrl; 
+
+  const _ImageBubble({
+    required this.imageUrl
+  }); 
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size; 
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
-          "https://yesno.wtf/assets/yes/13-c3082a998e7758be8e582276f35d1336.gif",
+          imageUrl,
           width: size.width * 0.7,
           height: 150,
           fit: BoxFit.cover,
           loadingBuilder: (context, child, loadingProgress) {
-            if(loadingProgress == null) return child; 
-            return Container(
-              width: size.width * 0.7,
-              height: 150,
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              child: const Text("Ana esta enviando una imagen..."),
-            ); 
-          },
-        ),
+          if(loadingProgress == null) return child; 
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: const Text("Ana esta enviando una imagen..."),
+          ); 
+        },
+      ),
     ); 
   }
 }
--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -1,37 +1,49 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  MessageFieldBox({super.key});
-
-  final textController = TextEditingController(); 
+  final ValueChanged<String> onValue;
+  const MessageFieldBox({
+    super.key,
+    required this.onValue
+  });
 
   @override
   Widget build(BuildContext context) {
-
-    //final colors = Theme.of(context).colorScheme; 
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
 
     final outlineInputBorder = UnderlineInputBorder(
-      borderSide:const BorderSide(color: Colors.transparent),
+      borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(40)
-    ); 
+    );
 
     final inputDecoration = InputDecoration(
-      hintText: 'End your message with a "??"',
+      hintText: 'End your message with a "?"',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffixIcon: IconButton(
         icon: const Icon(Icons.send_outlined),
-        onPressed: () {},
-      )
-    ); 
+        onPressed: () {
+          final textValue = textController.value.text;
+          onValue(textValue); 
+          textController.clear();
+        },
+      ),
+    );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
       controller: textController,
       decoration: inputDecoration,
-      onFieldSubmitted: ( value ) {
-        print(value); 
+      onFieldSubmitted: (value) {
+        focusNode.requestFocus();
+        onValue(value); 
+        textController.clear();
       },
-    ); 
+    );
   }
 }
